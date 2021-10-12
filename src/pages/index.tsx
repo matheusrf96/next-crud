97b5330e00c90d5af2b4a68c3b736c2bc845ff9d
Matheus Rodrigues <matheusrf96@gmail.com>
2021-10-12 10:34:18 -0300

    Implement layout and title components

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <div className={`
+      flex h-screen justify-center items-center
+      bg-gradient-to-r from-purple-500 to-blue-600
+      text-white
+    `}>
+      <Layout title="Cadastro Simples">
+        <span>Content</span>
+      </Layout>
+    </div>
+  )
+}
